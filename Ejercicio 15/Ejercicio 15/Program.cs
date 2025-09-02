2ec3affbcf436147f1573861f042c37e3cf86646
Luciano Alvarez <140111840+LUXI4NO@users.noreply.github.com>
2025-09-02 11:28:05 -0300

    Program.cs

--- Ejercicio 15/Ejercicio 15/Program.cs
+++ Ejercicio 15/Ejercicio 15/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Metodologia
 {
@@ -10,10 +10,11 @@ namespace Metodologia
         {
             Pila pila = new Pila();
             Cola cola = new Cola();
-            ColeccionMultiple multiple = new ColeccionMultiple(pila, cola);
 
             llenarAlumnos(pila);
             llenarAlumnos(cola);
+
+            ColeccionMultiple multiple = new ColeccionMultiple(pila, cola);
 
             Console.WriteLine("=== Pila ===");
             informar(pila);
@@ -32,14 +33,14 @@ namespace Metodologia
         {
             string[] nombres = { "Luciano", "Eliana", "Mateo", "Sebastian", "Carmen" };
 
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 string nombre = nombres[random.Next(nombres.Length)];
                 int dni = random.Next(20000000, 45000000);
                 int legajo = random.Next(1000, 5000);
                 double promedio = Math.Round(random.NextDouble() * 10, 2);
 
-                IComparable alumno = new Alumno(nombre, dni, legajo, promedio);
+                Alumno alumno = new Alumno(nombre, dni, legajo, promedio);
                 coleccionable.agregar(alumno);
             }
         }
@@ -51,22 +52,32 @@ namespace Metodologia
             IComparable minimo = coleccionable.minimo();
             IComparable maximo = coleccionable.maximo();
 
-            if(minimo != null && maximo != null)
+            if (minimo != null && maximo != null)
             {
                 Console.WriteLine("Elemento mínimo: " + minimo);
                 Console.WriteLine("Elemento máximo: " + maximo);
             }
 
-            Console.Write("Ingrese un DNI a buscar: ");
+            Console.Write("Ingrese un legajo a buscar: ");
             string input = Console.ReadLine();
-            int dni;
-            if(int.TryParse(input, out dni))
+            int legajo;
+            if (int.TryParse(input, out legajo))
             {
-                bool encontrado = coleccionable.contiene(new Alumno("", dni, 0, 0));
-                if(encontrado) Console.WriteLine("El alumno con DNI " + dni + " está en la colección.");
-                else Console.WriteLine("No se encontró el alumno con DNI " + dni + ".");
+                Alumno buscado = new Alumno("", 0, legajo, 0);
+                bool encontrado = coleccionable.contiene(buscado);
+                if (encontrado)
+                {
+                    Console.WriteLine("El alumno con legajo " + legajo + " está en la colección.");
+                }
+                else
+                {
+                    Console.WriteLine("No se encontró el alumno con legajo " + legajo + ".");
+                }
             }
-            else Console.WriteLine("Entrada inválida.");
+            else
+            {
+                Console.WriteLine("Entrada inválida.");
+            }
         }
     }
 }
